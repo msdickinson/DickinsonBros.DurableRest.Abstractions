9dd1b1dfa41ab4bd0cb15b9e6f0f8998790d79f5
Mark Dickinson <marksamdickinson@gmail.com>
2020-01-19 14:04:23 -0700

    Add project files.

---
+++ DickinsonBros.DurableRest.Abstractions/IDurableRestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DickinsonBros.DurableRest.Abstractions
+{
+    public interface IDurableRestService
+    {
+        Task<IRestResponse> ExecuteAsync(IRestClient client, IRestRequest restRequest, int retrys);
+        Task<IRestResponse<T>> ExecuteAsync<T>(IRestClient client, IRestRequest restRequest, int retrys);
+    }
+}
