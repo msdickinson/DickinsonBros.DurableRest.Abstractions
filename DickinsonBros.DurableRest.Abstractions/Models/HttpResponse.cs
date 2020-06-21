5a5f9a5e508d474862b6d27fb5f7c8cfaec6ee05
Mark Dickinson <marksamdickinson@gmail.com>
2020-06-21 14:47:25 -0700

    Removed Rest Sharp, And updated Interface

---
+++ DickinsonBros.DurableRest.Abstractions/Models/HttpResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+
+namespace DickinsonBros.DurableRest.Abstractions.Models
+{
+    public class HttpResponse<T>
+    {
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+        public T Data { get; set; }
+    }
+}
