8d3e69d8ab53d62d28d5e9ad2ded6fde3bcd120e
Mark Dickinson <marksamdickinson@gmail.com>
2020-06-21 14:50:45 -0700

    Update target framework (#3)
    * Update Target Framework
    
    * Removed Rest Sharp, And updated Interface

--- DickinsonBros.DurableRest.Abstractions/IDurableRestService.cs
+++ DickinsonBros.DurableRest.Abstractions/IDurableRestService.cs
@@ -1,11 +1,12 @@
-﻿using RestSharp;
+﻿using DickinsonBros.DurableRest.Abstractions.Models;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DickinsonBros.DurableRest.Abstractions
 {
     public interface IDurableRestService
     {
-        Task<IRestResponse> ExecuteAsync(IRestRequest restRequest, string baseURL, int retrys);
-        Task<IRestResponse<T>> ExecuteAsync<T>(IRestRequest restRequest, string baseURL, int retrys);
+        Task<HttpResponseMessage> ExecuteAsync(HttpClient httpClient, HttpRequestMessage httpRequestMessage, int retrys, double timeoutInSeconds);
+        Task<HttpResponse<T>> ExecuteAsync<T>(HttpClient httpClient, HttpRequestMessage httpRequestMessage, int retrys, double timeoutInSeconds);
     }
 }
